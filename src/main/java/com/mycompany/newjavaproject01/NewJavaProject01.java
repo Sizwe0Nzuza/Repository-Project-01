a17bee656f36afdc4440203fe2fdefaf7b6e8b2f
lab_services_student <lab_services_student@10.0.0.63>
2025-02-27 08:41:23 +0200

    New Java Project01 Fix this thing up brody

---
+++ src/main/java/com/mycompany/newjavaproject01/NewJavaProject01.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.newjavaproject01;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class NewJavaProject01 {
+
+    public static void main(String[] args) {
+      
+   Scanner  scanner = new Scanner (System.in); 
+   
+   System.out.println("Enter a Number01 ");
+   String Number01 = scanner.nextLine () ;
+   int Answer = Number01 + Number02 ;
+   System.out.println("Enter another Number");
+String Number02 = scanner.nextLine() ;
+
+
+
+System.out.println(" The Answer is =" );
+    }
+}
