0c2965ea71e70ec08c0559a4dfd6854b37b61aee
Sizwe0Nzuza <nzuzasizwe04@gmail.com>
2025-02-27 20:57:09 +0200

    Update NewJavaProject01.java (01)

--- src/main/java/com/mycompany/newjavaproject01/NewJavaProject01.java
+++ src/main/java/com/mycompany/newjavaproject01/NewJavaProject01.java
@@ -12,16 +12,33 @@ public class NewJavaProject01 {
 
     public static void main(String[] args) {
       
-   Scanner  scanner = new Scanner (System.in); 
-   
-   System.out.println("Enter a Number01 ");
-   String Number01 = scanner.nextLine () ;
-   int Answer = Number01 + Number02 ;
-   System.out.println("Enter another Number");
-String Number02 = scanner.nextLine() ;
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
 
+package com.mycompany.mavenproject1;
 
+/**
+ *
+ * @author 4nzuz
+ */
+public class Mavenproject1 {
 
-System.out.println(" The Answer is =" );
-    }
+    public static void main(String[] args) {
+     int Number01 = 50 ;
+     int Number02 = 10 ;
+     
+     int Answer = Number01 + Number02 ;
+     
+     int Number03 = 10 ;
+     int Number04 = 50 ;
+     int Answer02 = Number03 * Number04 ;
+     
+     System.out.println("The Awnser for Number01 + Number02 = " + Answer);
+     System.out.println("The Awnser for Number01 + Number 02 =" + Answer02) ;
+           
+    
+    
+}
+
 }
